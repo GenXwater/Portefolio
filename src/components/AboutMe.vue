8dc9d74d3c9ea4975e2daa4c81a3c348e8566d0d
GenXwater <louis.genty@outlook.com>
2026-01-29 10:13:11 +0100

    Add AboutMe component and integrate it into PortefolioMain

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="about-me">
+        <h2 class="text-highlight-1 section-title">Qui suis-je ?</h2>
+        <p class="default-text">
+            <span class="text-highlight-2">Profileur IA & developpeur d'applications</span>, je développe des <span class="text-highlight-2">applications web interactives</span>, avec une attention particulière portée aux <span class="text-highlight-2">usages</span>, aux <span class="text-highlight-2">comportements utilisateurs</span> et à la <span class="text-highlight-2">qualité de l’expérience</span>.<br>
+            Mon travail consiste à concevoir des <span class="text-highlight-2">interfaces</span> et des <span class="text-highlight-2">logiques applicatives claires</span>, reliées à des <span class="text-highlight-2">backends structurés</span>, et enrichies par des <span class="text-highlight-2">systèmes IA</span> (agents, RAG, automatisations) pensés pour être utiles, compréhensibles et responsables.<br>
+            À l’interface entre <span class="text-highlight-2">développement web</span> et <span class="text-highlight-2">IA appliquée</span>, je transforme des <span class="text-highlight-2">besoins concrets</span> en <span class="text-highlight-2">applications web fonctionnelles</span>, <span class="text-highlight-2">évolutives</span> et <span class="text-highlight-2">orientées produit</span>.
+        </p>
+    </div>
+</template>
+
+<style scoped>
+    #about-me {
+        margin-top: 80px;
+    }
+
+    .default-text {
+        text-align: justify;
+    }
+
+    .default-text span {
+        font-weight: bold;
+    }
+</style>
