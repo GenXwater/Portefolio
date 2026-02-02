4ec06081f8dd847ccb4e0e683dfc8a2004ed4da5
GenXwater <louis.genty@outlook.com>
2026-02-02 14:14:33 +0100

    feat: add redirects configuration for SPA support and update Vite config structure

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command }) => {
+  const plugins = [vue()]
+  // n'activer vueDevTools qu'en dev (commande "vite" / "serve")
+  if (command === 'serve') {
+    plugins.push(vueDevTools())
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
